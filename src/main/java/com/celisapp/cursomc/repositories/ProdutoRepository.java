b3af1f3c20d8abaee71c297a81b9f5ea6bf3f91e
Andre Celis <andreluiscelis@celisapp.com>
2020-04-25 00:08:08 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/celisapp/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.celisapp.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.celisapp.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
